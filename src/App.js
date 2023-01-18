e0c614cade7819accad6daa11ebcbf25d7af3d8f
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-01-18 17:07:55 +0100

    pass props to Question component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Question from './components/Question';
 
 function App() {
   return (
     <div className="App">
-
+      <Question 
+      text={"How would one say goodbye in Spanish?"}
+      firstOption={"Adios"}
+      secondOption={"Hola"}
+      thirdOption={"Au Revior"}
+      fourthOption={"Salir"}
+      />
     </div>
   );
 }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function Question(props) {
     return (
         <div className="question">
-            <span className="question--header"> How would one say goodbye in Spanish? </span>
-            <button className="question--button"> Adios </button>
-            <button className="question--button"> Hola </button>
-            <button className="question--button"> Au Revior </button>
-            <button className="question--button"> Salir </button>
+            <span className="question--header"> {props.text} </span>
+            <button className="question--button"> {props.firstOption} </button>
+            <button className="question--button"> {props.secondOption} </button>
+            <button className="question--button"> {props.thirdOption} </button>
+            <button className="question--button"> {props.fourthOption} </button>
         </div>
     )
 }
