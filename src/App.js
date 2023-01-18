c68a0de77f7c2b5d2f4f4a310f6b0b1055da17e0
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-01-18 17:45:40 +0100

    randomise answers order for each question

--- src/App.js
+++ src/App.js
@@ -9,15 +9,46 @@ function App() {
 
   const [questions, setQuestions] = React.useState(data.results)
 
-  const questionElements = questions.map(element => (
-    <Question 
-    text={element.question}
-    firstOption={element.correct_answer}
-    secondOption={element.incorrect_answers[0]}
-    thirdOption={element.incorrect_answers[1]}
-    fourthOption={element.incorrect_answers[2]}
-    />
-  ))
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+
+  const questionElements = questions.map(element => {
+    let answers = 
+    [element.correct_answer, 
+      element.incorrect_answers[0],
+      element.incorrect_answers[1], 
+      element.incorrect_answers[2]]
+
+    shuffle(answers)
+
+    return (
+      <Question 
+      text={element.question}
+      firstOption={answers[0]}
+      secondOption={answers[1]}
+      thirdOption={answers[2]}
+      fourthOption={answers[3]}
+      />
+    )
+  }
+
+  )
 
   return (
     <div className="App">
