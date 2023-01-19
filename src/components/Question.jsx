9c4612622db71df4cd015eac6f6586ee5b780d18
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-01-19 22:09:12 +0100

    rewrite question component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,10 +4,10 @@ export default function Question(props) {
     return (
         <div className="question">
             <span className="question--header"> {props.text} </span>
-            <button className="question--button" onClick={props.chooseAnswer}> {props.firstOption} </button>
-            <button className="question--button"> {props.secondOption} </button>
-            <button className="question--button"> {props.thirdOption} </button>
-            <button className="question--button"> {props.fourthOption} </button>
+            <button className="question--button" onClick={() => props.pickOption(props.firstOption)}> {props.firstOption} </button>
+            <button className="question--button" onClick={() => props.pickOption(props.secondOption)}>  {props.secondOption} </button>
+            <button className="question--button" onClick={() => props.pickOption(props.thirdOption)}> {props.thirdOption} </button>
+            <button className="question--button" onClick={() => props.pickOption(props.fourthOption)}> {props.fourthOption} </button>
         </div>
     )
 }
