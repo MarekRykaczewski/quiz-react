7f55f144b439c853839750f5a6fb4094b60c0241
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-01-20 16:14:19 +0100

    update styles in Answer component

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Answer(props) {
 
     const styles = {
-        backgroundColor: props.text === props.selected && "rgba(77, 91, 158, 1)",
-        color: props.text === props.selected && "white"
-
+        backgroundColor: props.text === props.selected && "rgb(166 180 245)",
+        color: props.text === props.selected && "rgb(50 63 125)",
+        border: props.text === props.selected && "2px solid rgb(166 180 245)"
     }
 
     return (
