22cc4dc3463d5289eaa65f220ead25bedd184523
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-01-22 17:41:30 +0100

    refactor for readability

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -5,14 +5,26 @@ export default function Question(props) {
     return (
         <div className="question">
             <span className="question--header"> {props.text} </span>
-            {/* <button className="question--button" onClick={() => props.pickOption(props.firstOption)}> {props.firstOption} </button>
-            <button className="question--button" onClick={() => props.pickOption(props.secondOption)}>  {props.secondOption} </button>
-            <button className="question--button" onClick={() => props.pickOption(props.thirdOption)}> {props.thirdOption} </button>
-            <button className="question--button" onClick={() => props.pickOption(props.fourthOption)}> {props.fourthOption} </button> */}
-            {props.firstOption && <Answer selected={props.selected} text={props.firstOption} pickOption={() => props.pickOption(props.id, props.firstOption)} />}
-            {props.secondOption && <Answer selected={props.selected} text={props.secondOption} pickOption={() => props.pickOption(props.id, props.secondOption)}/>}
-            {props.thirdOption && <Answer selected={props.selected} text={props.thirdOption} pickOption={() => props.pickOption(props.id, props.thirdOption)}/>}
-            {props.fourthOption && <Answer selected={props.selected} text={props.fourthOption} pickOption={() => props.pickOption(props.id, props.fourthOption)}/>}
+            {props.firstOption && 
+            <Answer 
+            selected={props.selected} 
+            text={props.firstOption} 
+            pickOption={() => props.pickOption(props.id, props.firstOption)} />}
+            {props.secondOption && 
+            <Answer 
+            selected={props.selected} 
+            text={props.secondOption} 
+            pickOption={() => props.pickOption(props.id, props.secondOption)}/>}
+            {props.thirdOption && 
+            <Answer 
+            selected={props.selected} 
+            text={props.thirdOption} 
+            pickOption={() => props.pickOption(props.id, props.thirdOption)}/>}
+            {props.fourthOption && 
+            <Answer 
+            selected={props.selected} 
+            text={props.fourthOption} 
+            pickOption={() => props.pickOption(props.id, props.fourthOption)}/>}
         </div>
     )
 }
